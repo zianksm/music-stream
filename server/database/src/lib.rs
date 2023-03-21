fd3d256d0245ebed41a3de7cb073404b3dc69f68
zian <mynameiszian@gmail.com>
2023-03-21 21:39:17 +0800

    fix dep

--- server/database/src/lib.rs
+++ server/database/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use sqlx::prelude::*;
+
+pub struct Database;
+
+impl Database {
+    pub async fn init() -> Self {
+        let pool = MyS 
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
