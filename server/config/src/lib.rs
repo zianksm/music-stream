ddb35880cb8d280864d58c7bfb051af83c3e45b3
zian <mynameiszian@gmail.com>
2023-03-21 21:59:35 +0800

    added db test

--- server/config/src/lib.rs
+++ server/config/src/lib.rs
@@ -1,16 +1,22 @@
 pub struct ServerConfig {
-    db_url: String,
+    db_local: String,
+    db_test: String,
 }
 
 impl ServerConfig {
     pub fn new() -> Self {
         dotenv::dotenv().ok();
 
-        let db_url = std::env::var("DB_URL").expect("missing db url");
+        let db_local = std::env::var("DB_LOCAL").expect("missing db local url");
+        let db_test = std::env::var("DB_TEST").expect("missing db test url");
 
         {
-            Self { db_url }
+            Self { db_local, db_test }
         }
+    }
+
+    pub fn db_url(&self) -> &str {
+        self.db_local.as_ref()
     }
 }
 
@@ -21,6 +27,6 @@ mod tests {
     #[test]
     fn it_should_load_env() {
         let result = ServerConfig::new();
-        assert!(!result.db_url.is_empty())
+        assert!(!result.db_local.is_empty())
     }
 }
--- server/database/src/lib.rs
+++ server/database/src/lib.rs
@@ -1,10 +1,31 @@
-use sqlx::prelude::*;
+use config::ServerConfig;
+use sqlx::{mysql, pool::PoolOptions, MySql, Pool};
 
-pub struct Database;
+pub struct Database {
+    config: ServerConfig,
+    pool: Pool<MySql>,
+}
 
 impl Database {
-    pub async fn init() -> Self {
-        let pool = MyS 
+    pub async fn init() -> Result<Self, sqlx::Error> {
+        let config = ServerConfig::new();
+
+        let pool = mysql::MySqlPoolOptions::new()
+            .max_connections(10)
+            .connect(config.db_url())
+            .await?;
+
+        let _self = Self { config, pool };
+
+        Ok(_self)
+    }
+
+    pub fn config(&self) -> &ServerConfig {
+        &self.config
+    }
+
+    pub fn pool(&self) -> &Pool<MySql> {
+        &self.pool
     }
 }
 
@@ -12,9 +33,9 @@ impl Database {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
