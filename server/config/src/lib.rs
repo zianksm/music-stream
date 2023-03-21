55267c0c2899139d4882b740a4d3172f801adc51
zian <mynameiszian@gmail.com>
2023-03-21 21:05:41 +0800

    added function to load env

--- server/config/src/lib.rs
+++ server/config/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+pub struct ServerConfig {
+    db_url: String,
+}
+
+impl ServerConfig {
+    pub fn new() -> Self {
+        dotenv::dotenv().ok();
+
+        let db_url = std::env::var("DB_URL").expect("missing db url");
+
+        {
+            Self { db_url }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +23,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_should_load_env() {
+        let result = ServerConfig::new();
+        assert!(!result.db_url.is_empty())
     }
 }
