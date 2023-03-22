30e65be344ce5c4df9f43babf73f120dd0f14efc
zian <mynameiszian@gmail.com>
2023-03-22 13:56:25 +0800

    simple api

--- server/driver/src/main.rs
+++ server/driver/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+
+mod api;
+
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| App::new().route("/", web::get().to(api::hello::hello)))
+        .bind("localhost:8080")
+        .unwrap()
+        .run()
+        .await
+        .unwrap();
 }
